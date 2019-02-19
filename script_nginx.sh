eac178e472c86c4547c874802b855c371067a702
luxiaochen <494985971@qq.com>
2019-02-20 00:39:06 +0800

    更新

--- script_nginx.sh
+++ script_nginx.sh
@@ -1,3 +1,4 @@
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
 sudo yum install yum-utils -y
 sudo sh -c "cat>/etc/yum.repos.d/nginx.repo"<<EOF
 [nginx-stable]
@@ -9,6 +10,8 @@ gpgkey=https://nginx.org/keys/nginx_signing.key
 EOF
 sudo yum install nginx -y
 sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
-sudo ln -s /vagrant/nginx/conf/default.conf /etc/nginx/conf.d/
+#sudo ln -s /vagrant/nginx/conf/default.conf /etc/nginx/conf.d/
+sudo cp /vagrant/nginx/conf/default.conf /etc/nginx/conf.d/
 sudo systemctl enable nginx.service 
 sudo systemctl start nginx.service
+sudo reboot
--- script_tomcat.sh
+++ script_tomcat.sh
@@ -2,3 +2,4 @@ sudo yum install tomcat tomcat-webapps tomcat-admin-webapps -y
 sudo systemctl enable tomcat
 sudo systemctl start tomcat
 sudo cp /vagrant/tomcat/webapps/test.war /usr/share/tomcat/webapps/
+sudo reboot
